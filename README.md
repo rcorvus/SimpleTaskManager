a9f846b07bcc2f76666621446f5b526fef4bd7f6
rcorvus <5025458+rcorvus@users.noreply.github.com>
2024-04-02 14:51:56 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SimpleTaskManager
+A simple task manager built with Python and Flask
+
+## add a .env file and add this line with your secret key:
+``` SECRET_KEY = 'my_secret_key' ```
---
+++ models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+def __repr__(self):
+    return f'{self.title} created on {self.date}'
+
+def __str__(self):
+    return f'{self.title} created on {self.date}'
